23a47daaac404d3e7a062a4423bd05cfb0c12800
Adekxyan <saputraadrian1259@gmail.com>
2024-07-10 09:15:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ cd data
 wget https://raw.githubusercontent.com/Adekjamannow/LunarCore/development/data/ActivityScheduling.json
 wget https://raw.githubusercontent.com/Adekjamannow/LunarCore/development/data/Banners.json
 wget https://raw.githubusercontent.com/Adekjamannow/LunarCore/development/data/RogueMapGen.json
-wget https://raw.githubusercontent.com/Melledy/LunarCore/development/src/main/java/emu/lunarcore/game/gacha/GachaService.java
-wget https://raw.githubusercontent.com/Melledy/LunarCore/development/src/main/java/emu/lunarcore/game/gacha/GachaBanner.java
+wget https://raw.githubusercontent.com/Adekjamannow/LunarCore/development/src/main/java/emu/lunarcore/game/gacha/GachaService.java
+wget https://raw.githubusercontent.com/Adekjamannow/LunarCore/development/src/main/java/emu/lunarcore/game/gacha/GachaBanner.java
 cd ..
 mkdir resources
 git clone --depth 1 https://github.com/Dimbreath/StarRailData
